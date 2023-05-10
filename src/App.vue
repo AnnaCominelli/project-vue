274b26c83c8a45fcda4e036a8c7b2d85d38fce3e
Anna <cominellianna98@gmail.com>
2023-05-10 11:32:55 +0200

    first version

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+/*import HelloWorld from './components/HelloWorld.vue'*/
+/*import TheWelcome from './components/TheWelcome.vue'*/
+import CustomComponent from './components/CustomComponent.vue'
+</script>
+
+<template>
+  <CustomComponent />
+<header>   
+  </header>
+
+<main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+}
+</style>
